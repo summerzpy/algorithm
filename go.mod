20d7bb95a3455bf71acd37db44128fbd26bd0ff6
summerzpy <zhaopeiyao52@163.com>
2021-11-13 11:00:23 +0800

    feat:add problem003

---
+++ go.mod
@@ -0,0 +1,1 @@
+module jz_offer
---
+++ problem003/003-数组中重复的数字.go
@@ -0,0 +1,26 @@
+package problem003
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param numbers int整型一维数组
+ * @return int整型
+ */
+// [2,3,1,0,2,5,3]
+func Duplicate(nums []int) int {
+	length := len(nums)
+	if length <= 0 {
+		return -1
+	}
+	for i := 0; i < length; i++ {
+		for nums[i] != i {
+			if nums[nums[i]] == nums[i] {
+				return nums[i]
+			}
+			nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+		}
+	}
+	return -1
+}
+
